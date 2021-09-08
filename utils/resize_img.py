d5cd503b7c71fa9f4ef9593c2733bea87b4772a3
mrcv20 <marcos.valentee20@gmail.com>
2021-09-07 21:13:28 -0400

    function resize img

---
+++ utils/resize_img.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image
+from django.conf import settings
+
+@staticmethod
+def resize_image(img, new_width=500):
+    img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+    img_pill = Image.open(img_full_path)
+    # desempacotamento
+    original_width, original_height = img_pill.size
+
+    if original_width <= new_width:
+        print("original > new_width")
+        img_pill.close
+        return
+
+    new_height = round((new_width * original_height) / original_width)
+    new_img = img_pill.resize((new_width, new_height), Image.LANCZOS )
+    new_img.save(
+        img_full_path,
+        optimize=True,
+        quality=50
+    )
