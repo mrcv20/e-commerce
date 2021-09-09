b12347a7675d3f2f45c288500ae5397bec915cbe
mrcv20 <marcos.valentee20@gmail.com>
2021-09-08 22:11:29 -0400

    models for profiles

--- profil/models.py
+++ profil/models.py
@@ -1,3 +1,63 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+import re
+from utils.validacpf import valida_cpf
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField()
+    address = models.CharField(max_length=50)
+    number = models.CharField(max_length=5)
+    complement = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cpf = models.CharField(max_length=11)
+    neighbor = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(
+        max_length=2,
+        default='AM',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+        )
+    )
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    def clean(self):
+        error_messages = {}
+        if not valida_cpf(self.cpf):
+            error_messages['cpf'] = 'Digite um cpf válido'
+
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messages['cep'] = 'Cep invalido'
+
+        if error_messages:
+            raise ValidationError(error_messages)
