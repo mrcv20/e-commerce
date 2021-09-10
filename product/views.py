86ebcf99d3d50f736208afa096dd507482eeebc6
mrcv20 <marcos.valentee20@gmail.com>
2021-09-09 23:37:28 -0400

    created views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class ProductList(ListView):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Lista de produto')
+
+class ProductDetail(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe produto')
+
+
+class AddToCart(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adiciona do carrinh')
+
+
+class RemoveToCart(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remove do carrinho')
+
+
+class Cart(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho1')
+
+
+class Finish(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('FIinish')
--- request/views.py
+++ request/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+
+class Pay(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('pay')
+
+
+class CloseOrder(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('close')
+
+
+class Detail(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('details')
